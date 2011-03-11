a342d32296034ad2cb8b743cc99d4c9b1f23bb8b
evil_k <evil_k@7258aa16-79bd-4fdc-8f83-50ad731c2ab6>
2011-03-11 23:59:25 +0000

    really really dirty hack to get some sense out of the .add files
    git-svn-id: svn://svn.code.sf.net/p/readesm/code@185 7258aa16-79bd-4fdc-8f83-50ad731c2ab6

--- fileformat/vuBlocks/vuFactory.cpp
+++ fileformat/vuBlocks/vuFactory.cpp
@@ -6,12 +6,13 @@
 #include "vuSpeeds.h"
 #include "vuTechnical.h"
 #include "vuHexBlock.h"
+#include "vu11Block.h"
 
 QSharedPointer<vuBlock> vuFactory(constDataPointer start) {
 	typedef QSharedPointer<vuBlock> p;
 	if(start[0] != 0x76) {
 		qDebug("Not a block known to readesm, does not begin with 0x76");
-		return p();
+		return p(new vuHexBlock(start));
 	}
 	switch(start[1]) {
 		case vuOverview::TREP:
@@ -24,6 +25,10 @@ QSharedPointer<vuBlock> vuFactory(constDataPointer start) {
 			return p(new vuSpeeds(start));
 		case vuTechnical::TREP:
 			return p(new vuTechnical(start));
+		case vu11Block::TREP:
+			return p(new vu11Block(start));
+		case vu13Block::TREP:
+			return p(new vu13Block(start));
 		default:
 			qDebug("Oops! Unknown vu block %d", start[1]);
 			return p(new vuHexBlock(start));
