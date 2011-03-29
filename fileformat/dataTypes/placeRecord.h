1ea66eed47b0e201a93489d5e96e5b93b60c17ab
evil_k <evil_k@7258aa16-79bd-4fdc-8f83-50ad731c2ab6>
2011-03-29 17:40:10 +0000

    new files from refactoring
    git-svn-id: svn://svn.code.sf.net/p/readesm/code@200 7258aa16-79bd-4fdc-8f83-50ad731c2ab6

---
+++ fileformat/dataTypes/placeRecord.h
@@ -0,0 +1,39 @@
+#ifndef PLACERECORD_H
+#define PLACERECORD_H
+
+#include "dataType.h"
+
+struct placeRecord {
+	Q_DECLARE_TR_FUNCTIONS(placeRecord)
+public:
+	Time entryTime;
+	int entryTypeDailyWorkPeriod;
+	int dailyWorkPeriodCountry;
+	int dailyWorkPeriodRegion;
+	int vehicleOdometerValue;
+
+	placeRecord(constDataPointer start) :
+		entryTime(BEInt32(start)), 
+		entryTypeDailyWorkPeriod(start[4]),
+		dailyWorkPeriodCountry(start[5]), 
+		dailyWorkPeriodRegion(start[6]), 
+		vehicleOdometerValue(BEInt24(start + 7))
+	{
+	}
+
+	friend reporter& operator<<(reporter& report, placeRecord sub) {
+		report(tr("entryTime"), sub.entryTime.str());
+		report(tr("entryTypeDailyWorkPeriod"), formatStrings::dailyWorkPeriod(
+				sub.entryTypeDailyWorkPeriod));
+		report(tr("dailyWorkPeriodCountry"), formatStrings::nationNumeric(sub.dailyWorkPeriodCountry));
+		if(sub.dailyWorkPeriodRegion) 
+			report(tr("dailyWorkPeriodRegion"), sub.dailyWorkPeriodRegion);
+		report(tr("vehicleOdometerValue"), sub.vehicleOdometerValue);
+		return report;
+	}
+	static bool defval(constDataPointer start) {
+		return checkZeros(start, 10);
+	}
+};
+
+#endif
