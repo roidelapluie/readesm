f6354c06574897fa792f271581045fc091e5ecbf
evil_k <evil_k@7258aa16-79bd-4fdc-8f83-50ad731c2ab6>
2011-03-30 20:47:50 +0000

    graphs now have a legend
    git-svn-id: svn://svn.code.sf.net/p/readesm/code@211 7258aa16-79bd-4fdc-8f83-50ad731c2ab6

--- fileformat/picgen.cpp
+++ fileformat/picgen.cpp
@@ -1,19 +1,36 @@
 #include "picgen.h"
 
-void drawDayOutlineStart(QTextStream& o)
-{
-	o << "<svg xmlns='http://www.w3.org/2000/svg' width='740' height='120'>";
-	o << "<g transform='translate(10,0)'>";
+QString svgGraph::drawBorderAndTimeAxis() const {
+	QString rv;
+	QTextStream o(&rv);
 	o << "<g style='text-anchor:middle;font-size:16px;'>";
 	for(int j = 0; j < 25; ++j)
-		o << "<text x='" << (j * 30) << "' y='118'>" << j
+		o << "\n\t<text x='" << (j * 30) << "' y='118'>" << j
 				<< "</text><line x1='" << (j * 30) << "' y1='100' x2='" << (j
 				* 30) << "' y2='104' style='stroke-width:2;stroke:black' />";
 	o << "</g>";
 	o << "<polyline points='0,0 720,0 720,100 0,100 0,0' style='fill:none;stroke:black;stroke-width:2'/>";
+	return rv;
 }
 
-void drawDayOutlineEnd(QTextStream& o)
-{
-	o << "</g></svg>";
+QString svgBarGraph::drawLegend() const{
+	return QString(
+		"\t\t<g style='text-anchor:left;font-size:16px;'>"
+		"\t\t\t<rect x='730' y='86' fill='red' width='30' height='16' /><text x='765' y='100'>%1</text>"
+		"\t\t\t<rect x='730' y='66' fill='blue' width='30' height='16' /><text x='765' y='80'>%2</text>"
+		"\t\t\t<rect x='730' y='46' fill='yellow' width='30' height='16' /><text x='765' y='60'>%3</text>"
+		"\t\t\t<rect x='730' y='26' fill='green' width='30' height='16' /><text x='765' y='40'>%4</text>"
+		"\t\t</g>"
+		)
+		.arg(tr("= break/rest"))
+		.arg(tr("= break, up to 15 min"))
+		.arg(tr("= working"))
+		.arg(tr("= driving"));
+}
+
+QString svgGraph::addSvgHull(QString content, int width) const { 
+	return QString("<svg xmlns='http://www.w3.org/2000/svg' width='%1' height='135'>\n\t<g transform='translate(10,0)'>")
+		.arg(width)
+	+ content
+	+ "\n\t</g>\n</svg>";
 }
--- fileformat/picgen.h
+++ fileformat/picgen.h
@@ -19,21 +19,18 @@
 
 #include <QtCore/QString>
 #include <QtCore/QTextStream>
-
+#include <QCoreApplication>
 class picgen {
 	protected:
 		QString collected;
 	public:
 		QTextStream collector;
 	picgen() : collected(), collector(&collected) {}
-	virtual QString str() {
+	virtual QString str() const{
 		return collected;
 	}
 	virtual void add(int from, int duration, int height, QString color, QString title) {}
 };
-
-void drawDayOutlineStart(QTextStream& o);
-void drawDayOutlineEnd(QTextStream& o);
 
 class htmlBarGraph : public picgen {
 	static const int compressh = 2;
@@ -43,44 +40,39 @@ class htmlBarGraph : public picgen {
 				/ compressh) << "' height='" << height << "' title='" << title
 				<< "' alt='" << title << "'/>";
 	}
-	virtual QString str() {
-		QString rv;
-		QTextStream o(&rv); 
-		o << str()
-				<< "<img src='images/scale.gif' height='20' width='" << (1440
-				/ compressh) << "' alt='scale' />";
-		return rv;
+	virtual QString str() const {
+		return collected + QString("<img src='images/scale.gif' height='20' width='%1' alt='scale' />").arg(1440 / compressh);
 	}
 };
 
-class svgBarGraph : public picgen {
-	public:
+class svgGraph : public picgen {
+	Q_DECLARE_TR_FUNCTIONS(svgGraph)
+public:
+	QString addSvgHull(QString content, int width) const;
+	QString drawBorderAndTimeAxis() const;
+	virtual QString str() const {
+		return addSvgHull(collected, 740);
+	}
+};
+
+class svgBarGraph : public svgGraph {
+	Q_DECLARE_TR_FUNCTIONS(svgBarGraph)
+public:
+	QString drawLegend() const;
 	virtual void add(int from, int duration, int height, QString color, QString title) {
-		collector<< "<rect x='" << from << "' fill='" << color << "' width='"
+		collector<< "\n\t\t\t<rect x='" << from << "' fill='" << color << "' width='"
 				<< duration << "' height='" << height << "' title='" << title
-				<< "'><title>"<< title << "</title></rect>\n";
+				<< "'><title>"<< title << "</title></rect>";
 	}
-	virtual QString str() {
-		QString rv;
-		QTextStream o(&rv); 
-		drawDayOutlineStart(o);
-		o << "<g transform='scale(0.5,-1) translate(0,-100)'>"
-				<< collected << "</g>";
-		drawDayOutlineEnd(o);
-		return rv;
+	virtual QString str() const{
+		return addSvgHull("\n\t\t<g transform='scale(0.5,-1) translate(0,-100)'>" + collected + "\n\t\t</g>" + drawBorderAndTimeAxis() + drawLegend(), 900);
 	}
 };
 
-class svgPlotGraph : public picgen {
+class svgPlotGraph : public svgGraph {
 	public:
-	virtual QString str() {
-		QString rv;
-		QTextStream o(&rv); 
-		drawDayOutlineStart(o);
-		o << "<g transform='scale(0.0083333,-1) translate(0,-100)'><path style='stroke:#dd2200' d='M 0 0 L "
-		<< collected << "' /></g>";
-		drawDayOutlineEnd(o);
-		return rv;
+	virtual QString str() const{
+		return addSvgHull("<g transform='scale(0.0083333,-1) translate(0,-100)'><path style='stroke:#dd2200' d='M 0 0 L " + collected + "' /></g>" + drawBorderAndTimeAxis(), 740);
 	}
 };
 
