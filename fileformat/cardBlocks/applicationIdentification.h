89c5ef050d67a2a76735fd3f03efffc9011c66d3
evil_k <evil_k@7258aa16-79bd-4fdc-8f83-50ad731c2ab6>
2011-04-07 14:08:43 +0000

    read application_identification fields
    git-svn-id: svn://svn.code.sf.net/p/readesm/code@248 7258aa16-79bd-4fdc-8f83-50ad731c2ab6

--- fileformat/cardBlocks/applicationIdentification.h
+++ fileformat/cardBlocks/applicationIdentification.h
@@ -11,17 +11,33 @@ class applicationIdentification : public cardBlock {
 	QString name() const {
 		return tr("Application Identification");
 	}
-	int typeOfTachographCardId, cardStructureVersion;
+	int typeOfTachographCardId;
+	int cardStructureVersion;
+	int noOfEventsPerType;
+	int noOfFaultsPerType;
+	int activityStructureLength;
+	int noOfCardVehicleRecords;
+	int noOfCardPlaceRecords;
+	
 	applicationIdentification(constDataPointer filewalker) :
 		cardBlock(filewalker), 
 		typeOfTachographCardId(start[5]),
-		cardStructureVersion(BEInt16(start + 6)) 
+		cardStructureVersion(BEInt16(start + 6)),
+		noOfEventsPerType(start[8]),
+		noOfFaultsPerType(start[9]),
+		activityStructureLength(BEInt16(start + 10)),
+		noOfCardVehicleRecords(BEInt16(start + 12)),
+		noOfCardPlaceRecords(start[5])
 	{
 	}
 	virtual void printOn(reporter& o) const {
 		o(tr("typeOfTachographCardId"), formatStrings::equipmentType(typeOfTachographCardId));
 		o(tr("cardStructureVersion"), hex(cardStructureVersion, 4));
-		//TODO: read noOfCompanyActivityRecords
+		o(tr("noOfEventsPerType"), noOfEventsPerType);
+		o(tr("noOfFaultsPerType"), noOfFaultsPerType);
+		o(tr("activityStructureLength"), activityStructureLength);
+		o(tr("noOfCardVehicleRecords"), noOfCardVehicleRecords);
+		o(tr("noOfCardPlaceRecords"), noOfCardPlaceRecords);
 	}
 };
 
