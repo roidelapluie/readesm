551fa736851ac6b2ca6b4f10324c59224b3016ea
evil_k <evil_k@7258aa16-79bd-4fdc-8f83-50ad731c2ab6>
2011-08-18 10:15:07 +0000

    do not display timestamp 0 time
    git-svn-id: svn://svn.code.sf.net/p/readesm/code@268 7258aa16-79bd-4fdc-8f83-50ad731c2ab6

--- fileformat/DataTypes/TimeReal.cpp
+++ fileformat/DataTypes/TimeReal.cpp
@@ -8,6 +8,7 @@ TimeReal::TimeReal(const constDataPointer& start) : QDateTime(QDateTime::fromTim
 QString TimeReal::toString() const{
 	QTime t = time();
 	if(t.hour() == 0 && t.minute() == 0 && t.second() == 0){
+		if(date().year() == 1970 && date().month() == 1 && date().day() == 1) return tr("undefined");
 		return date().toString();
 	}
 	return QDateTime::toString();
--- fileformat/DataTypes/TimeReal.h
+++ fileformat/DataTypes/TimeReal.h
@@ -5,8 +5,10 @@
 #include "../readTypes.h"
 
 #include <QtCore/QDateTime>
+#include <QtCore/QCoreApplication>
 
 class TimeReal : public QDateTime{
+	Q_DECLARE_TR_FUNCTIONS(TimeReal)
 	public:
 	TimeReal(const constDataPointer& start);
 	QString toString() const;
