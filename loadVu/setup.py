150fceaa115ed84089608b93de5f0c282a76da2b
evil_k <evil_k@7258aa16-79bd-4fdc-8f83-50ad731c2ab6>
2011-08-24 14:13:05 +0000

    moved script to subfolder
    git-svn-id: svn://svn.code.sf.net/p/readesm/code@288 7258aa16-79bd-4fdc-8f83-50ad731c2ab6

---
+++ loadVu/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name='loadVu',
+	version='0.1',
+	py_modules=[],
+	scripts=['loadVu'],
+	author='Andreas Gölzer',
+	author_email='andreas@goelzer.de',
+	data_files=[],
+	url='http://readesm.sf.net/',
+	long_description='New trucks in the European Union are required to have a digital tachograph unit on board.  This script downloads data from vehicle units attached to the computer with a serial cable.',
+	license='GPL 3+'
+)
