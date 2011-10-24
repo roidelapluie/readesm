1df93d977e46b60bf96f83a6d16dc2c0ed7119e9
evil_k <evil_k@7258aa16-79bd-4fdc-8f83-50ad731c2ab6>
2011-10-24 19:45:38 +0000

    split cardactivitydailyrecord .h/.cpp
    git-svn-id: svn://svn.code.sf.net/p/readesm/code@319 7258aa16-79bd-4fdc-8f83-50ad731c2ab6

---
+++ fileformat/DataTypes/CardActivityDailyRecord.cpp
@@ -0,0 +1,44 @@
+#include "CardActivityDailyRecord.h"
+
+#include <QtCore/QDebug>
+#include "../PictureGenerators/SvgDayActivity.h"
+
+CardActivityDailyRecord::CardActivityDailyRecord(const DataPointer& start) : RawCardActivityDailyRecord(start),
+	activityChangeInfos(Subblocks<ActivityChangeInfo>::fromTypeAndLength(start + RawCardActivityDailyRecord::staticSize, activityRecordLength - RawCardActivityDailyRecord::staticSize))
+{
+	if(activityChangeInfos.numberOfBlocks() > 0){
+		for(int j = 0; j < activityChangeInfos.numberOfBlocks() - 1; ++j){
+			if(activityChangeInfos[j + 1].t != 0) activityChangeInfos[j].setDuration(activityChangeInfos[j + 1].t - activityChangeInfos[j].t, false);
+			else activityChangeInfos[j].setDuration(60*24 - activityChangeInfos[j].t, false);
+		}
+		activityChangeInfos[activityChangeInfos.numberOfBlocks() - 1].setDuration(60*24 - activityChangeInfos[activityChangeInfos.numberOfBlocks() - 1].t, false);
+	}
+}
+
+int CardActivityDailyRecord::size() const {
+	if(activityRecordLength < RawCardActivityDailyRecord::staticSize){
+		qDebug() << "Size to small in CardActivityDailyRecord";
+		return RawCardActivityDailyRecord::staticSize;
+	}
+	if(activityRecordLength > RawCardActivityDailyRecord::staticSize + 2 * 60 * 24){
+		qDebug() << "Size of CardActivityDailyRecord excessive:" <<  activityRecordLength;
+	}
+	return activityRecordLength;
+}
+
+void CardActivityDailyRecord::printOn(Reporter& o) const {
+	RawCardActivityDailyRecord::printOn(o);
+	if(o.allowSvg()){
+		SvgDayActivity visualization;
+		for(int j = 0; j < activityChangeInfos.numberOfBlocks(); ++j){
+			visualization.add(activityChangeInfos[j].t, activityChangeInfos[j].duration, activityChangeInfos[j].heightHint(), activityChangeInfos[j].color(), activityChangeInfos[j].toString());
+		}
+		o.tagValuePair(tr("Visualization"), visualization.toString());
+	}
+
+	o.writeArray(activityChangeInfos, tr("activityChangeInfos"), false);
+}
+
+QString CardActivityDailyRecord::title() const {
+	return tr("Activities on %1").arg(activityRecordDate.toString());
+}
--- fileformat/DataTypes/CardActivityDailyRecord.h
+++ fileformat/DataTypes/CardActivityDailyRecord.h
@@ -5,49 +5,15 @@
 #include "ActivityChangeInfo.h"
 #include "RawCardActivityDailyRecord.h"
 
-#include <QtCore/QDebug>
-#include "../PictureGenerators/SvgDayActivity.h"
 
 class CardActivityDailyRecord : public RawCardActivityDailyRecord {
 public:
 	Subblocks<ActivityChangeInfo> activityChangeInfos;
 	
-	CardActivityDailyRecord(const DataPointer& start) : RawCardActivityDailyRecord(start),
-		activityChangeInfos(Subblocks<ActivityChangeInfo>::fromTypeAndLength(start + RawCardActivityDailyRecord::staticSize, activityRecordLength - RawCardActivityDailyRecord::staticSize))
-	{
-		if(activityChangeInfos.numberOfBlocks() > 0){
-			for(int j = 0; j < activityChangeInfos.numberOfBlocks() - 1; ++j){
-				if(activityChangeInfos[j + 1].t != 0) activityChangeInfos[j].setDuration(activityChangeInfos[j + 1].t - activityChangeInfos[j].t, false);
-				else activityChangeInfos[j].setDuration(60*24 - activityChangeInfos[j].t, false);
-			}
-			activityChangeInfos[activityChangeInfos.numberOfBlocks() - 1].setDuration(60*24 - activityChangeInfos[activityChangeInfos.numberOfBlocks() - 1].t, false);
-		}
-	}
-	int size() const {
-		if(activityRecordLength < RawCardActivityDailyRecord::staticSize){
-			qDebug() << "Size to small in CardActivityDailyRecord";
-			return RawCardActivityDailyRecord::staticSize;
-		}
-		if(activityRecordLength > RawCardActivityDailyRecord::staticSize + 2 * 60 * 24){
-			qDebug() << "Size of CardActivityDailyRecord excessive:" <<  activityRecordLength;
-		}
-		return activityRecordLength;
-	}
-	void printOn(Reporter& o) const {
-		RawCardActivityDailyRecord::printOn(o);
-		if(o.allowSvg()){
-			SvgDayActivity visualization;
-			for(int j = 0; j < activityChangeInfos.numberOfBlocks(); ++j){
-				visualization.add(activityChangeInfos[j].t, activityChangeInfos[j].duration, activityChangeInfos[j].heightHint(), activityChangeInfos[j].color(), activityChangeInfos[j].toString());
-			}
-			o.tagValuePair(tr("Visualization"), visualization.toString());
-		}
-
-		o.writeArray(activityChangeInfos, tr("activityChangeInfos"), false);
-	}
-	QString title() const {
-		return tr("Activities on %1").arg(activityRecordDate.toString());
-	}
+	CardActivityDailyRecord(const DataPointer& start);
+	int size() const;
+	void printOn(Reporter& o) const;
+	QString title() const;
 };
 
 #endif
