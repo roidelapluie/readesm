96611fda24f83cb94e38907c1d1791b64611318b
evil_k <evil_k@7258aa16-79bd-4fdc-8f83-50ad731c2ab6>
2011-12-23 10:14:40 +0000

    setup changes
    git-svn-id: svn://svn.code.sf.net/p/readesm/code@380 7258aa16-79bd-4fdc-8f83-50ad731c2ab6

--- loadvu/setup.py
+++ loadvu/setup.py
@@ -5,12 +5,14 @@ from distutils.core import setup
 setup(
 	name='loadvu',
 	version='2011.11',
-	py_modules=['vuserial','timestampsutc'],
+	py_modules=['vuserial','timestampsutc','loadvugui'],
 	scripts=['loadvu'],
 	author='Andreas Gölzer',
 	author_email='andreas@goelzer.de',
 	data_files=[],
 	url='http://readesm.sf.net/',
 	long_description='New trucks in the European Union are required to have a digital tachograph unit on board.  This script downloads data from vehicle units attached to the computer with a serial cable.',
-	license='GPL 3+'
+	license='GPL 3+',
+	requires=['PyQt4','serial'],
+	classifiers=['Programming Language :: Python']
 )
